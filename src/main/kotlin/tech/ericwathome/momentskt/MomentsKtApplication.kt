3dd0fa49c0634b052bbe9e1e3fa93b9716055942
Eric Wafula <wathomeeric@gmail.com>
2022-07-06 14:43:24 +0300

    initial commit

---
+++ src/main/kotlin/tech/ericwathome/momentskt/MomentsKtApplication.kt
@@ -0,0 +1,11 @@
+package tech.ericwathome.momentskt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MomentsKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<MomentsKtApplication>(*args)
+}
---
+++ src/test/kotlin/tech/ericwathome/momentskt/MomentsKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.ericwathome.momentskt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MomentsKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
